43a10828fb55cdbfd4c44f2a911a9c1829f1ee3b
Artem <vebulogmetra@yandex.ru>
2023-04-18 18:35:15 +0300

    add CRUD ops for User

--- api/handlers.py
+++ api/handlers.py
@@ -25,6 +25,19 @@ async def _create_new_user(data: UserCreate, db) -> GetUser:
                            is_active=user.is_active,)
 
 
+async def _check_exists_by_email(email: str, db) -> bool:
+    async with db as session:
+        async with session.begin():
+            user_crud = UserCRUD(session)
+            result = await user_crud.user_exists(email=email)
+            is_exists: bool = result["anon_1"]
+            return is_exists
+
+
 @user_router.post("/create", response_model=GetUser)
 async def create_user(data: UserCreate, db: AsyncSession = Depends(get_db)):
     return await _create_new_user(data=data, db=db)
+
+@user_router.post("/check_exists")
+async def check_email_exists(email: str, db: AsyncSession = Depends(get_db)):
+    return await _check_exists_by_email(email=email, db=db)
--- db/models.py
+++ db/models.py
@@ -1,4 +1,4 @@
-import uuid
+from uuid import uuid4
 from sqlalchemy import Column, String, Boolean
 from sqlalchemy.dialects.postgresql import UUID
 from db.session import Base
@@ -7,7 +7,7 @@ from db.session import Base
 class User(Base):
     __tablename__ = "users"
 
-    user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     username = Column(String, nullable=False)
     name = Column(String, nullable=False)
     surname = Column(String, nullable=True)
